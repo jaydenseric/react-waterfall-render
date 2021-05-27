296eb84105b675c6cd497b9834713f05ba0ceeed
Jayden Seric <me@jaydenseric.com>
2021-05-27 11:10:58 +1000

    Rename variables in the test index module.

--- changelog.md
+++ changelog.md
@@ -5,6 +5,7 @@
 ### Patch
 
 - Updated dev dependencies.
+- Renamed variables in the test index module.
 
 ## 2.0.0
 
--- test/index.mjs
+++ test/index.mjs
@@ -1,12 +1,12 @@
 import TestDirector from 'test-director';
-import testWaterfallRenderContext from './WaterfallRenderContext.test.mjs';
-import testBundle from './bundle.test.mjs';
-import testWaterfallRender from './waterfallRender.test.mjs';
+import test_WaterfallRenderContext from './WaterfallRenderContext.test.mjs';
+import test_bundle from './bundle.test.mjs';
+import test_waterfallRender from './waterfallRender.test.mjs';
 
 const tests = new TestDirector();
 
-testWaterfallRender(tests);
-testWaterfallRenderContext(tests);
-testBundle(tests);
+test_waterfallRender(tests);
+test_WaterfallRenderContext(tests);
+test_bundle(tests);
 
 tests.run();
